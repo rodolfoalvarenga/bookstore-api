eab1359dfb1e663d1df2d52c3eabffb4f22a9c8d
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-26 12:19:14 -0300

    Dev profile created

---
+++ src/main/java/com/rodolfo/bookstore/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.rodolfo.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rodolfo.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciaBaseDeDados() {
+		if (strategy.equals("create")) {
+			this.dbService.instanciarBaseDeDados();
+		}
+		return false;
+	}
+}
