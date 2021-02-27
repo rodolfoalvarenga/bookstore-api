cc0d54940b3cf0b2136375c5cab14dfffcd11ce8
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-27 12:33:03 -0300

    PUT for Categoria

--- src/main/java/com/rodolfo/bookstore/service/CategoriaService.java
+++ src/main/java/com/rodolfo/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodolfo.bookstore.domain.Categoria;
+import com.rodolfo.bookstore.dtos.CategoriaDTO;
 import com.rodolfo.bookstore.repositories.CategoriaRepository;
 import com.rodolfo.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
