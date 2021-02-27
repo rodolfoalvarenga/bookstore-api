4868ba9b8535ad11433479d21a7b987a7586e949
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-27 11:21:53 -0300

    Customized exception for object not found

--- src/main/java/com/rodolfo/bookstore/service/CategoriaService.java
+++ src/main/java/com/rodolfo/bookstore/service/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.rodolfo.bookstore.domain.Categoria;
 import com.rodolfo.bookstore.repositories.CategoriaRepository;
+import com.rodolfo.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
 	}
 }
